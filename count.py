a6f55f6ba9f985eeb11c99c54097fd9e519f8e6e
Biki <102693150+Bikash1075@users.noreply.github.com>
2022-11-23 19:05:23 +0530

    Add files via upload

--- count.py
+++ count.py
@@ -1,12 +1,11 @@
-numbers=(1,2,3,4,5,6,7,8,9)
-len_number= len(numbers)
 even_number=0
 odd_number=0
-for i in range(len_number):
+n= int(input("Enter a number "))
+for i in range(1,n+1):
     if i%2==0:
-        even_number=even_number+1
+        even_number = even_number+1
     else:
-        odd_number=odd_number+1
+        odd_number = odd_number+1
 
 print("Number of even numbers:",even_number)
 print("Number of odd numbers:",odd_number)
---
+++ mirror dimension.py
@@ -0,0 +1,22 @@
+string = input("Enter a String ")
+reverse_string = ''
+for i in string:
+    reverse_string= i+reverse_string
+    print(reverse_string)
+print(reverse_string)
+
+s= input("enter a string ")
+rev="".join(reversed(s))
+print(rev)
+
+
+
+string= input("Enter a String ")
+rev=string[-1::-1]
+print(rev)
+
+string= input("Enter a String ")
+rev= lambda x:x[::-1]
+print(rev(string))
+
+
